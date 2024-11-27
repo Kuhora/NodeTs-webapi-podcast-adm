83214865363c8b54b63fd2416782d9358363cfb7
kuhora <kuhora123@gmail.com>
2024-11-27 15:31:04 -0300

    Server created

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+import * as http from "http";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {});
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
